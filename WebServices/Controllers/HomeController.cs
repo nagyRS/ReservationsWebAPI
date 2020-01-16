5ee86b9dbfef1079072d3f8b30178a48a12d95d4
Reka Nagy <Reka Nagy>
2020-01-16 10:53:08 +0000

    Add project files.

---
+++ WebServices/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    // MVC 5 empty controller
+    //Each of the action methods corresponds directly to one of the methods in the repository
+
+        // the controller only adds : model validation, selecting views, and performing redirections
+        
+        // Action methods are removed as the Web API controller replaced them
+
+    public class HomeController : Controller
+    {
+       
+        // GET: Home
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+
+
+        //private ReservationRepository repo = new ReservationRepository.Current;
+
+        //public ViewResult Index()
+        //{
+        //    return View(repo.GetAll());
+        //}
+
+        //public ActionResult Add(Reservation item)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        repo.Add(item);
+        //        return RedirectToAction("Index");
+        //    }
+        //    else
+        //    {
+        //        return View("Index");
+        //    }
+        //}
+
+        //public ActionResult Remove(int id)
+        //{
+        //    repo.Remove(id);
+        //    return RedirectToAction("Index");
+        //}
+
+        //public ActionResult Update(Reservation item)
+        //{
+        //    if (ModelState.IsValid && repo.Update(item))
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    else
+        //    {
+        //        return View("Index");
+        //    }
+        //}
+    }
+}
---
+++ WebServices/Controllers/WebController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    // after creating the API controller, call localhost/api/web for XML or JSON  <api/web(contoller)/id>
+
+    // the URL will produce a list of all the model objects
+
+    public class WebController : ApiController
+    {
+        private ReservationRepository repo = ReservationRepository.Current;
+
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repo.GetAll();
+        }
+
+        public Reservation GetReservation(int id)
+        {
+            return repo.Get(id);
+        }
+
+
+        [HttpPost]                                                     //added attribute
+        public Reservation CreateReservation(Reservation item)      //changed the method name form PostReservation
+        {
+            return repo.Add(item);
+        }
+
+        [HttpPut]
+        public bool UpdateReservation(Reservation item)             // changed the name from PutReservation
+        {
+            return repo.Update(item);
+        }
+
+        public void DeleteReservation(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
